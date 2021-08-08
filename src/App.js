c77417e055016d009d40e10d017c423febb9dd2c
Ana.Sousa <anasousa314@outlook.com>
2021-08-08 13:14:20 -0300

    aula06 criando o nosso primeiro componente

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+// Functional Component (Componente Funcional) - forma mais moderna
+export default function App(){
+  return <h1>Componente App</h1>;
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
+import App from './App.js'
+
 
 ReactDOM.render(
-  <h1 id="title">Hello World!</h1>,
+  <App />,
   document.getElementById('root'),
 );
