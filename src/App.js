a6d7b139fb551081e0fa857a9015671d2f942361
Ana.Sousa <anasousa314@outlook.com>
2021-08-08 13:29:47 -0300

    aula08 expressoes js no jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-// Functional Component (Componente Funcional) - forma mais moderna
+const category = 'Posts da semana';
+
 export default function App(){
   return (
     <>
-      <h1>Componente App</h1>
-      <h2>Subtítulo</h2>
+      <h1>JStack's Blog{console.log('Expressão dentro do JSX')}</h1>
+      <h2>{category}</h2>
     </>
   );
 }
@@ -14,3 +15,5 @@ export default function App(){
 Podemos importar o React.Fragment, a difereça de se usar dessa forma '<React.Fragment></React.Fragment>' para essa forma '<></>' é no primeiro caso podemos colocar atributos no fragment por exemplo um className, mas no segundo caso isso não é permitido.
 Elementos adjacentes são irmãos. 
 */
+
+// Render -> Renderizar
