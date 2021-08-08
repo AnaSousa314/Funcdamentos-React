233758b5f8e596c92229a2df41d11b14f80df3b6
Ana.Sousa <anasousa314@outlook.com>
2021-08-08 13:23:42 -0300

    aula07 sobre React Fragment

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from 'react';
 
 // Functional Component (Componente Funcional) - forma mais moderna
 export default function App(){
-  return <h1>Componente App</h1>;
+  return (
+    <>
+      <h1>Componente App</h1>
+      <h2>Subtítulo</h2>
+    </>
+  );
 }
+
+/* 
+Podemos importar o React.Fragment, a difereça de se usar dessa forma '<React.Fragment></React.Fragment>' para essa forma '<></>' é no primeiro caso podemos colocar atributos no fragment por exemplo um className, mas no segundo caso isso não é permitido.
+Elementos adjacentes são irmãos. 
+*/
