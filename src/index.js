fc7c7094fd67646110abec2bb731dd6e1101ccb4
Ana <anasousa314@outlook.com>
2021-11-17 11:53:04 -0300

    aula22 introducao ao styled components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
-import App from './App.js'
-import './index.scss'
+import App from './components/App'
+// import './index.scss'
+import GlobalStyle from './styles/global'
 
 ReactDOM.render(
-  <App />,
+  <>
+    <GlobalStyle/>
+    <App />
+  </>,
   document.getElementById('root'),
 );
