c24982fd2ba27d1e4dd196a9728c33f5be310751
Ana.Sousa <anasousa314@outlook.com>
2021-12-20 15:49:31 -0300

    aula27 dicas de navegacao entre compenentes no vscode

--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,7 +1,7 @@
 import styled, {css} from 'styled-components';
 
 export const Container = styled.article`
-    margin-bottom: 24px;
+    margin-bottom: 32px;
 
     /* opacity: ${(props)=>props.removed ? 0.5 : 1};
     color: ${(props)=>props.removed ? "#f00" : "#fff"} */
